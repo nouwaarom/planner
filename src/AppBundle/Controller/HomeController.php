bb8b9fb8525bc4cb5fa24b3f9044c812ce2dc189
Elbert <elbert@t-matix.nl>
2014-11-27 23:01:35 +0100

    Used annotations for routing

--- src/AppBundle/Controller/HomeController.php
+++ src/AppBundle/Controller/HomeController.php
@@ -3,9 +3,13 @@
 namespace AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
 class HomeController extends Controller
 {
+    /**
+     * @Route("/", name="homepage")
+     */
     public function showAction()
     {
         return $this->render('homepage.html.twig');
